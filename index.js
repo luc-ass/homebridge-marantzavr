7531a198ca3e03272a4b713ea7373d7dda8381cc
Lucas Gasenzer <lucasgasenzer@mac.com>
2015-11-05 20:39:58 +0100

    beautify...

--- index.js
+++ index.js
@@ -12,92 +12,91 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   
-  homebridge.registerAccessory("homebridge-marantzavr", "marantzavr", MarantzAccessory);
+  homebridge.registerAccessory("homebridge-marantzavr", "MarantzAVR", MarantzAVR);
 
 
-function MarantzAccessory(log, config) {
-	// configuration
-	this.ip = config['ip'];
-	this.name = config['name'];
-	this.http_method = "GET";
+  function MarantzAVR(log, config) {
+    // configuration
+    this.ip = config['ip'];
+    this.name = config['name'];
+    this.http_method = "GET";
 
-	this.log = log;
+    this.log = log;
 
-	this.on_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutZone_OnOff/ON";
-	this.off_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutZone_OnOff/OFF";
+    this.on_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutZone_OnOff/ON";
+    this.off_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutZone_OnOff/OFF";
 
-	this.mute_on = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutVolumeMute/ON";
-	this.mute_off = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutVolumeMute/OFF";
+    this.mute_on = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutVolumeMute/ON";
+    this.mute_off = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutVolumeMute/OFF";
 
-	this.volume_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutMasterVolumeSet/";
-}
+    this.volume_url = "http://" + this.ip + "/MainZone/index.put.asp?cmd0=PutMasterVolumeSet/";
+  }
 
-// Custom Characteristics and service...
+  // Custom Characteristics and service...
+  MarantzAVR.AudioVolume = function() {
+    Characteristic.call(this, 'Volume', '00001001-0000-1000-8000-135D67EC4377');
+    this.setProps({
+      format: Characteristic.Formats.FLOAT,
+      //unit: Characteristic.Units.PERCENTAGE,
+      maxValue: 35,
+      minValue: -80,
+      minStep: 1,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(MarantzAVR.AudioVolume, Characteristic);
 
-MarantzAccessory.AudioVolume = function() {
-  Characteristic.call(this, 'Volume', '00001001-0000-1000-8000-135D67EC4377');
-  this.setProps({
-    format: Characteristic.Formats.FLOAT,
-    //unit: Characteristic.Units.PERCENTAGE,
-    maxValue: 35,
-    minValue: -80,
-    minStep: 1,
-    perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-  });
-  this.value = this.getDefaultValue();
-};
-inherits(MarantzAccessory.AudioVolume, Characteristic);
+  MarantzAVR.Muting = function() {
+    Characteristic.call(this, 'Mute', '00001002-0000-1000-8000-135D67EC4377');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(MarantzAVR.Muting, Characteristic);
 
-MarantzAccessory.Muting = function() {
-  Characteristic.call(this, 'Mute', '00001002-0000-1000-8000-135D67EC4377');
-  this.setProps({
-    format: Characteristic.Formats.BOOL,
-    perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-  });
-  this.value = this.getDefaultValue();
-};
-inherits(MarantzAccessory.Muting, Characteristic);
+  MarantzAVR.AudioDeviceService = function(displayName, subtype) {
+    Service.call(this, displayName, '00000001-0000-1000-8000-135D67EC4377', subtype);
 
-MarantzAccessory.AudioDeviceService = function(displayName, subtype) {
-  Service.call(this, displayName, '00000001-0000-1000-8000-135D67EC4377', subtype);
+    // Required Characteristics
+    this.addCharacteristic(MarantzAVR.AudioVolume);
 
-  // Required Characteristics
-  this.addCharacteristic(MarantzAccessory.AudioVolume);
+    // Optional Characteristics
+    //this.addOptionalCharacteristic(MarantzAVR.Muting);
+    this.addCharacteristic(MarantzAVR.Muting);
+  };
+  inherits(MarantzAVR.AudioDeviceService, Service);
 
-  // Optional Characteristics
-  //this.addOptionalCharacteristic(MarantzAccessory.Muting);
-  this.addCharacteristic(MarantzAccessory.Muting);
-};
-inherits(MarantzAccessory.AudioDeviceService, Service);
+  MarantzAVR.prototype = {
 
-MarantzAccessory.prototype = {
+    httpRequest: function(url, method, callback) {
+		  var that = this;
 
-	httpRequest: function(url, method, callback) {
-		var that = this;
+		  request({
+        url: url,
+        method: method
+		  },
+		  function (error, response, body) {
+        callback(error, response, body)
+      })
+    },
 
-		request({
-			url: url,
-			method: method
-		},
-		function (error, response, body) {
-			callback(error, response, body)
-		})
-	},
+    getPowerState: function(callback) {
+      callback(true);
+    },
 
-	getPowerState: function(callback) {
-		callback(true);
-	},
+    setPowerState: function(powerOn, callback) {
+      var url;
 
-	setPowerState: function(powerOn, callback) {
-    	var url;
-
-    	if (powerOn) {
-      		url = this.on_url;
-      		this.log("Set", this.name, "to on");
+      if (powerOn) {
+        url = this.on_url;
+      	this.log("Set", this.name, "to on");
     	}
     	else {
-      		url = this.off_url;
-      		this.log("Set", this.name, "to off");
+      	url = this.off_url;
+      	this.log("Set", this.name, "to off");
     	}
 
     	this.httpRequest(url, this.http_method, function(error, response, body) {
@@ -112,20 +111,20 @@ MarantzAccessory.prototype = {
     	}.bind(this));
   	},
 
-	getMuteState: function(callback) {
-		callback(true);
-	},
+    getMuteState: function(callback) {
+      callback(true);
+    },
 
-  	setMuteState: function(muteOn, callback) {
+    setMuteState: function(muteOn, callback) {
     	var url;
 
     	if (muteOn) {
-      		url = this.mute_on;
-      		this.log(this.name, "muted");
+      	url = this.mute_on;
+      	this.log(this.name, "muted");
     	}
     	else {
-      		url = this.mute_off;
-      		this.log(this.name, "unmuted");
+      	url = this.mute_off;
+      	this.log(this.name, "unmuted");
     	}
 
     	this.httpRequest(url, this.http_method, function(error, response, body) {
@@ -149,7 +148,7 @@ MarantzAccessory.prototype = {
   		callback();
   	},
 
-	getServices: function() {
+  getServices: function() {
 		var that = this;
 		
 		var informationService = new Service.AccessoryInformation();
@@ -165,20 +164,20 @@ MarantzAccessory.prototype = {
 				.on('get', this.getPowerState.bind(this))
 				.on('set', this.setPowerState.bind(this));
 
-		var audioDeviceServie = new MarantzAccessory.AudioDeviceService("Audio Functions");
+		var audioDeviceServie = new MarantzAVR.AudioDeviceService("Audio Functions");
 		audioDeviceServie
-			.getCharacteristic(MarantzAccessory.Muting)
+			.getCharacteristic(MarantzAVR.Muting)
 				.on('get', this.getMuteState.bind(this))
 				.on('set', this.setMuteState.bind(this));
 
 		audioDeviceServie
-			.getCharacteristic(MarantzAccessory.AudioVolume)
+			.getCharacteristic(MarantzAVR.AudioVolume)
 				.on('get', this.getVolume.bind(this))
 				.on('set', this.setVolume.bind(this));
 
 		// not yet
 		// var audioDeviceServie = new Service. // what do we do now?
-	return [informationService, switchService, audioDeviceServie];
-	}
-}
+    return [informationService, switchService, audioDeviceServie];
+    }
+  }
 }
